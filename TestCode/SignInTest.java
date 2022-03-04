4290bc1b257147812ddda98823057e29e5543440
robwarhurstUni <99671014+robwarhurstUni@users.noreply.github.com>
2022-03-04 15:31:34 +0000

    Create SignInTest.java
    Test code for Customer Class - saveCust() and custSignIn().

---
+++ TestCode/SignInTest.java
@@ -0,0 +1,108 @@
+package codeTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.junit.jupiter.api.Test;
+
+import code.*;
+
+class SignInTest {
+	
+	String url = "jdbc:oracle:thin:OPS$2042387/P46919@ora-srv.wlv.ac.uk:1521/catdb.wlv.ac.uk";
+	
+	Connection con = null;
+
+	// Test to save a new customer and confirm it is in the database.
+	@Test
+	void testSaveCust() {
+
+			// Create and save a new customer.
+			Name new_name = new Name("Billy Idol");
+			Customer new_cust = new Customer(new_name, "Password123", "12 White Wedding Street"
+											, "RebelYell@hotmail.com", "07756468712");
+			
+			new_cust.saveCust();
+			
+			int email_count = 0;
+			
+			try {
+			       DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+			       con = DriverManager.getConnection(url);
+			       
+			       // Query to find count of all emails matching 'RebelYell@hotmail.com',
+			       Statement stmt = con.createStatement();
+			       
+			       ResultSet rs = stmt.executeQuery("SELECT COUNT(*) AS count FROM Frackhub_Test WHERE email = 'RebelYell@hotmail.com'");  
+
+			       while (rs.next()) {
+			    	   email_count = rs.getInt("count");
+			       }	       
+			       
+			       // Execute deletion of all rows with email 'RebelYell@hotmail.com'.
+			       Statement stmt2 = con.createStatement();
+			       
+			       stmt2.executeUpdate("DELETE FROM Frackhub_Test WHERE email = 'RebelYell@hotmail.com'");			       
+			       
+			       con.close();
+			       
+			   }
+
+			catch (Exception ex) {
+		
+			       System.err.println(ex);
+			   }
+			
+			// Check there is exactly 1 count of email when customer added.
+			assertEquals(1, email_count);
+
+	}
+
+	// Test to check sign in will get the correct customer information from the database.
+	@Test
+	void testCustSignIn() {
+		
+		// Create and save a new customer.
+		Name new_name = new Name("Billy Idol");
+		Customer new_cust = new Customer(new_name, "Password123", "12 White Wedding Street"
+										, "RebelYell@hotmail.com", "07756468712");
+		
+		new_cust.saveCust();
+		
+		Customer test_cust = new Customer();
+
+		// Sign in with email and password.
+		test_cust = test_cust.custSignIn("RebelYell@hotmail.com", "Password123");
+		
+		// Check all the customer details are the same.
+		assertEquals(test_cust.getName().getFullName(), new_cust.getName().getFullName());
+		assertEquals(test_cust.getAddress(), new_cust.getAddress());
+		assertEquals(test_cust.getEmail(), new_cust.getEmail());
+		assertEquals(test_cust.getPassword(), new_cust.getPassword());
+		assertEquals(test_cust.getPhone_num(), new_cust.getPhone_num());
+		
+		// Execute deletion of all rows with email 'RebelYell@hotmail.com'.
+		try {
+		       DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+		       con = DriverManager.getConnection(url);
+
+		       Statement stmt = con.createStatement();
+		       
+		       stmt.executeUpdate("DELETE FROM Frackhub_Test WHERE email = 'RebelYell@hotmail.com'");			       
+		       
+		       con.close();
+		       
+		   }
+
+		catch (Exception ex) {
+	
+		       System.err.println(ex);
+		   }
+		
+	}
+
+}
