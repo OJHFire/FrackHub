eff2766592cb05a3daf89cba55d49b6e2794832b
robwarhurstUni <99671014+robwarhurstUni@users.noreply.github.com>
2022-03-12 13:26:36 +0000

    Add files via upload
    Change to stop being able to book own items.

---
+++ MainJava/fracHub/src/codeTest/BookingTest.java
@@ -0,0 +1,19 @@
+package codeTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class BookingTest {
+
+	@Test
+	void testSaveBooking() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCheckBooking() {
+		fail("Not yet implemented");
+	}
+
+}
