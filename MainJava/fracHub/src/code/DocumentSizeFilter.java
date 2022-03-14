20ba3c26f0943d4dba89edcf0f0e1cca1cc8c294
Gursimran280 <99670887+Gursimran280@users.noreply.github.com>
2022-03-14 15:37:21 +0000

    Add files via upload

---
+++ MainJava/fracHub/src/code/DocumentSizeFilter.java
@@ -0,0 +1,13 @@
+//Code from Oracle
+import javax.swing.*;
+import javax.swing.text.*;
+
+
+public class DocumentSizeFilter extends DocumentFilter {
+	int maxChars;
+	
+	public DocumentSizeFilter(int maxChars) {
+		this.maxChars = maxChars;
+	}
+
+}
--- MainJava/fracHub/src/code/newItemGUI.java
+++ MainJava/fracHub/src/code/newItemGUI.java
@@ -1,9 +1,13 @@
-package code;
-
 // Call this function from the Main Menu sending the userName and the User ID
 
 import javax.swing.*;
+
 import javax.swing.border.EmptyBorder;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.text.DefaultStyledDocument;
+import javax.swing.text.Document;
+import javax.swing.text.DocumentFilter;
 
 import java.awt.*;
 import java.awt.event.*;
@@ -13,18 +17,23 @@ import java.sql.PreparedStatement;
 import java.text.Format;
 
 public class newItemGUI implements ActionListener {
-	
+
 	JFrame frame;
-	
+
 	String userName;
+
+	String itemName;
+	String itemType;
+	String itemDescription;
 	double value;
 	double costPerDay;
 	int userId;
-	
+
 	JPanel contentPane;
 	JPanel topPanel;
 	JPanel centerPanel;
 	JPanel bottomPanel;
+	JPanel warningPanel;
 
 	JButton btnSubmit;
 	JButton btnBack;
@@ -32,13 +41,13 @@ public class newItemGUI implements ActionListener {
 	JTextField txtFieldItemName = new JTextField();
 	JComboBox<String> comboItemType = new JComboBox<String>();
 	JTextArea textAreaDescription = new JTextArea();
+	DefaultStyledDocument doc;
 	JTextField txtFieldValue = new JTextField();
 	JTextField txtFieldCostPerDay = new JTextField();
-	
+
 	GridBagLayout gbl_topPanel;
 	GridBagConstraints gbc_lblTitle;
-	GridBagConstraints gbc_lblUser;
-	
+
 	GridBagLayout gbl_centerPanel;
 	GridBagConstraints gbc_lblItemName;
 	GridBagConstraints gbc_txtFieldItemName;
@@ -50,48 +59,42 @@ public class newItemGUI implements ActionListener {
 	GridBagConstraints gbc_txtFieldValue;
 	GridBagConstraints gbc_lblCostPerDay;
 	GridBagConstraints gbc_txtFieldCostPerDay;
-	
-	
-	
-	JLabel lblTitle = new JLabel("FRACKHUB");
-	JLabel lblUser = new JLabel("User");
+	GridBagConstraints gbc_warning;
+
+	JLabel lblTitle = new JLabel("ITEM REGISTRATION");
 	JLabel lblItemName = new JLabel("Item Name");
 	JLabel lblItemType = new JLabel("Item Type");
 	JLabel lblDescription = new JLabel("Description");
 	JLabel lblValue = new JLabel("Value");
 	JLabel lblCostPerDay = new JLabel("Cost per Day");
-	
-	newItemGUI(String name, int id){
+	int count;
+
+	newItemGUI(String name, int id) {
 		this.userName = name;
 		this.userId = id;
-		newItem();
-		
+		newItem(new JFrame());
+
 	}
-	
 
-	public void newItem() {
+	public void newItem(JFrame new_Frame) {
 
-		frame = new JFrame();
+		frame = new_Frame;
 		frame.setVisible(true);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setBounds(100, 100, 871, 358);
-		
-		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
-		frame.setContentPane(contentPane);
-		
-		//TOP PANEL
+
+		// 50, 50 , 450, 500
+		frame.setBounds(50, 50, 450, 500);
+
+		// TOP PANEL
 		topPanel = new JPanel();
-		contentPane.add(topPanel, BorderLayout.NORTH);
 		gbl_topPanel = new GridBagLayout();
-		gbl_topPanel.columnWidths = new int[] { 280, 280, 280, 0 };
+		gbl_topPanel.columnWidths = new int[] { 50, 10, 30, 20 };
 		gbl_topPanel.rowHeights = new int[] { 14, 0 };
 		gbl_topPanel.columnWeights = new double[] { 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		gbl_topPanel.rowWeights = new double[] { 0.0, Double.MIN_VALUE };
 		topPanel.setLayout(gbl_topPanel);
-		
-		//Title
+
+		// Title
 		lblTitle.setFont(new Font("Serif", Font.BOLD, 30));
 		gbc_lblTitle = new GridBagConstraints();
 		gbc_lblTitle.fill = GridBagConstraints.VERTICAL;
@@ -99,33 +102,23 @@ public class newItemGUI implements ActionListener {
 		gbc_lblTitle.gridx = 1;
 		gbc_lblTitle.gridy = 0;
 		topPanel.add(lblTitle, gbc_lblTitle);
-		
-		//User
-		lblUser.setText("Welcome: " + userName);
-		gbc_lblUser = new GridBagConstraints();
-		gbc_lblUser.anchor = GridBagConstraints.EAST;
-		gbc_lblUser.fill = GridBagConstraints.VERTICAL;
-		gbc_lblUser.gridx = 2;
-		gbc_lblUser.gridy = 0;
-		topPanel.add(lblUser, gbc_lblUser);
-		
-		//CENTER PANEL
+
+		// CENTER PANEL
 		centerPanel = new JPanel();
-		contentPane.add(centerPanel, BorderLayout.CENTER);
 		gbl_centerPanel = new GridBagLayout();
-		gbl_centerPanel.columnWidths = new int[] { 150, 180, 180, 150, 0 };
-		gbl_centerPanel.rowHeights = new int[] { 14, 20, 0, 0, 0, 0, 0, 0 };
+		gbl_centerPanel.columnWidths = new int[] { 50, 20, 0, 20 };
+		gbl_centerPanel.rowHeights = new int[] { 14, 5, 0, 0, 0, 0, 0, 0 };
 		gbl_centerPanel.columnWeights = new double[] { 0.0, 1.0, 1.0, 0.0, Double.MIN_VALUE };
 		gbl_centerPanel.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		centerPanel.setLayout(gbl_centerPanel);
-		
+
 		// Item Name Label
 		gbc_lblItemName = new GridBagConstraints();
 		gbc_lblItemName.insets = new Insets(0, 0, 5, 5);
 		gbc_lblItemName.gridx = 1;
 		gbc_lblItemName.gridy = 1;
 		centerPanel.add(lblItemName, gbc_lblItemName);
-		
+
 		// Item Name Field
 		txtFieldItemName.setToolTipText("Enter Item Name");
 		txtFieldItemName.setForeground(Color.BLACK);
@@ -136,62 +129,87 @@ public class newItemGUI implements ActionListener {
 		gbc_txtFieldItemName.gridy = 1;
 		centerPanel.add(txtFieldItemName, gbc_txtFieldItemName);
 		txtFieldItemName.setColumns(10);
-		
+
 		gbc_lblItemType = new GridBagConstraints();
 		gbc_lblItemType.insets = new Insets(0, 0, 5, 5);
 		gbc_lblItemType.gridx = 1;
 		gbc_lblItemType.gridy = 2;
 		centerPanel.add(lblItemType, gbc_lblItemType);
-		
-		//Item Type
+
+		// Item Type
 		comboItemType.setBackground(Color.WHITE);
 		comboItemType.setModel(
-				new DefaultComboBoxModel<String>(new String[] { "Electricronic", "Non-Electricronic", "Book", "Digital" }));
+				new DefaultComboBoxModel<String>(new String[] { "Electronic", "Non-Electronic", "Book", "Digital" }));
 		gbc_comboItemType = new GridBagConstraints();
 		gbc_comboItemType.insets = new Insets(0, 0, 5, 5);
 		gbc_comboItemType.fill = GridBagConstraints.HORIZONTAL;
 		gbc_comboItemType.gridx = 2;
 		gbc_comboItemType.gridy = 2;
 		centerPanel.add(comboItemType, gbc_comboItemType);
-		
-		//Item Description
+
+		// Item Description
 		gbc_lblDescription = new GridBagConstraints();
 		gbc_lblDescription.insets = new Insets(0, 0, 5, 5);
 		gbc_lblDescription.gridx = 1;
 		gbc_lblDescription.gridy = 3;
 		centerPanel.add(lblDescription, gbc_lblDescription);
-		
+
 		textAreaDescription.setToolTipText("Enter Item Description");
 		textAreaDescription.setLineWrap(true);
+		doc = new DefaultStyledDocument();
+		doc.setDocumentFilter(new DocumentSizeFilter(100));
+		doc.addDocumentListener(new DocumentListener() {
+			@Override
+			public void changedUpdate(DocumentEvent e) {
+				updateCount();
+			}
+
+			@Override
+			public void insertUpdate(DocumentEvent e) {
+				updateCount();
+			}
+
+			@Override
+			public void removeUpdate(DocumentEvent e) {
+				updateCount();
+			}
+
+		});
+
+		textAreaDescription.setDocument(doc);
+
+		count = updateCount();
+
 		gbc_textAreaDescription = new GridBagConstraints();
 		gbc_textAreaDescription.insets = new Insets(0, 0, 5, 5);
 		gbc_textAreaDescription.fill = GridBagConstraints.HORIZONTAL;
 		gbc_textAreaDescription.gridx = 2;
 		gbc_textAreaDescription.gridy = 3;
+
 		centerPanel.add(textAreaDescription, gbc_textAreaDescription);
-		
-		//Value
+
+		// Value
 		gbc_lblValue = new GridBagConstraints();
 		gbc_lblValue.insets = new Insets(0, 0, 5, 5);
 		gbc_lblValue.gridx = 1;
 		gbc_lblValue.gridy = 4;
 		centerPanel.add(lblValue, gbc_lblValue);
-		
+
 		gbc_txtFieldValue = new GridBagConstraints();
 		gbc_txtFieldValue.insets = new Insets(0, 0, 5, 5);
 		gbc_txtFieldValue.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtFieldValue.gridx = 2;
 		gbc_txtFieldValue.gridy = 4;
 		centerPanel.add(txtFieldValue, gbc_txtFieldValue);
-		
-		//Label Cost per Day
+
+		// Label Cost per Day
 		gbc_lblCostPerDay = new GridBagConstraints();
 		gbc_lblCostPerDay.insets = new Insets(0, 0, 5, 5);
 		gbc_lblCostPerDay.gridx = 1;
 		gbc_lblCostPerDay.gridy = 5;
 		centerPanel.add(lblCostPerDay, gbc_lblCostPerDay);
-		
-		//Cost per Day Field
+
+		// Cost per Day Field
 		txtFieldCostPerDay = new JTextField();
 		gbc_txtFieldCostPerDay = new GridBagConstraints();
 		gbc_txtFieldCostPerDay.insets = new Insets(0, 0, 5, 5);
@@ -200,28 +218,100 @@ public class newItemGUI implements ActionListener {
 		gbc_txtFieldCostPerDay.gridy = 5;
 		centerPanel.add(txtFieldCostPerDay, gbc_txtFieldCostPerDay);
 		
-		//Bottom Panel
+		
+
+		// Bottom Panel
 		bottomPanel = new JPanel();
-		contentPane.add(bottomPanel, BorderLayout.SOUTH);
 		bottomPanel.setLayout(new FlowLayout(FlowLayout.CENTER, 150, 5));
-		
-		
 
 		btnSubmit = new JButton("Submit");
 		btnSubmit.addActionListener(this);
 		btnBack = new JButton("Back");
 		btnBack.addActionListener(this);
-		
+
 		bottomPanel.add(btnSubmit);
 		bottomPanel.add(btnBack);
-		
-		
 
+		frame.getContentPane().removeAll();
+		frame.getContentPane().add(topPanel, BorderLayout.NORTH);
+		frame.getContentPane().add(centerPanel, BorderLayout.CENTER);
+		frame.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
+
+		frame.repaint();
+		frame.revalidate();
 
 	}
-	
-	public void addToDatabase(double value, double costPerDay ) {
+
+	private int updateCount() {
+		count = 100 - doc.getLength();
+
+		// if count < 0 show warning Panel
+
+		return count;
+	}
+
+	public void conNewItem() {
+		if ((!txtFieldItemName.getText().isEmpty()) && (count < 100) && (!txtFieldValue.getText().isEmpty())
+				&& (!txtFieldCostPerDay.getText().isEmpty())) {
+			
+			itemName = txtFieldItemName.getText();
+			itemType = comboItemType.getSelectedItem().toString();
+			
+			if (count > 0) {
+				//Save Description
+				itemDescription = textAreaDescription.getText();
+				
+				if(checkNumeric(txtFieldValue.getText())) {
+					//Save value of Item
+					value = Double.parseDouble(txtFieldValue.getText());
+					
+					if(checkNumeric(txtFieldCostPerDay.getText())) {
+						//Save Cost per Day of the Item
+						costPerDay = Double.parseDouble(txtFieldCostPerDay.getText());
+						//Add to database
+						System.out.println("All Details Submitted");
+						
+						
+					}else 
+					{
+						newItem(frame);
+						inputWarning("Invalid item Cost Per Day");
+					}
+				}else 
+				{
+					newItem(frame);
+					inputWarning("Invalid item Value");
+				}
+
+			}else 
+			{
+				newItem(frame);
+				inputWarning("Desctiption exceeds 100 words");
+			}
+		}
+		else {
+			newItem(frame);
+			inputWarning("Compile all the fields");
+		}
+	}
+
+	public void inputWarning(String message) {
+
+
+		// Warning
+		JLabel lblWarning = new JLabel(message);
 		
+		gbc_warning = new GridBagConstraints();
+		gbc_warning.insets = new Insets(0, 5, 5, 5);
+		gbc_warning.gridx = 2;
+		gbc_warning.gridy = 6;
+		centerPanel.add(lblWarning, gbc_warning);
+		frame.repaint();
+		frame.revalidate();
+	}
+
+	public void addToDatabase(double value, double costPerDay) {
+
 		try {
 			System.out.println("Item Registration");
 			// Registering drivers
@@ -231,8 +321,8 @@ public class newItemGUI implements ActionListener {
 					.getConnection("jdbc:oracle:thin:OPS$2042387/P46919@ora-srv.wlv.ac.uk:1521/catdb.wlv.ac.uk");
 			System.out.println("Connected with database");
 			// Prepared Statement
-			PreparedStatement Pstatement = connection
-					.prepareStatement("insert into Items(Id, UserID, Name, Type, Description, Value, Borrow_Cost) values(seq_item.nextval,?,?,?,?,?,?)");
+			PreparedStatement Pstatement = connection.prepareStatement(
+					"insert into Items(Id, UserID, Name, Type, Description, Value, Borrow_Cost) values(seq_item.nextval,?,?,?,?,?,?)");
 			// Specifying the values of it's parameter
 			Pstatement.setInt(1, userId);
 			System.out.println("userId: Done");
@@ -246,31 +336,37 @@ public class newItemGUI implements ActionListener {
 			System.out.println("Value: Done");
 			Pstatement.setDouble(6, costPerDay);
 			System.out.println("Cost per Day: Done");
-			
+
 			Pstatement.executeUpdate();
 
 		} catch (Exception e1) {
 			e1.printStackTrace();
 		}
-		
-		
-		
+
+	}
+
+	public boolean checkNumeric(String s) {
+		try {
+			Double.parseDouble(s);
+			return true;
+
+		} catch (NumberFormatException error) {
+			return false;
+		}
 	}
 
 	public void actionPerformed(ActionEvent e) {
 
 		if (e.getSource() == btnSubmit) {
 			try {
-				value = Double.parseDouble(txtFieldValue.getText());
-				System.out.println("Value: " + userName);
-				costPerDay = Double.parseDouble(txtFieldCostPerDay.getText());
-				System.out.println("Cost Per Day: " + costPerDay);
-				addToDatabase(value, costPerDay );
-			} catch(Exception error) {
-				System.out.println("INVALID DATA\n Please check the Value and the Cost Per Day");			}
-			
+				conNewItem();
+				// addToDatabase(value, costPerDay);
+			} catch (Exception error) {
+				System.out.println("INVALID DATA\n Please check the Value and the Cost Per Day");
+			}
+
 		} else if (e.getSource() == btnBack) {
-			//Main Menu
+			// Main Menu
 			System.out.println("BackToMenu");
 		}
 	}
