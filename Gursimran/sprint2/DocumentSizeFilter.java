68d6131117526199e932162e7fe7573213703faf
Gursimran280 <99670887+Gursimran280@users.noreply.github.com>
2022-04-25 14:34:13 +0100

    Create DocumentSizeFilter.java

---
+++ Gursimran/sprint2/DocumentSizeFilter.java
@@ -0,0 +1,14 @@
+package Test;
+//Code from Oracle
+import javax.swing.*;
+import javax.swing.text.*;
+
+
+public class DocumentSizeFilter extends DocumentFilter {
+	int maxChars;
+	
+	public DocumentSizeFilter(int maxChars) {
+		this.maxChars = maxChars;
+	}
+
+}
